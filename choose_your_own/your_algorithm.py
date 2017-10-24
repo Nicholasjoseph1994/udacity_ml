e9585a2eaf0824331aeaf888de3fca524839791b
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2017-10-24 11:17:29 -0400

    try ensemble of different methods

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,9 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -14,6 +17,26 @@ grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if l
 bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+clf1 = AdaBoostClassifier()
+clf1.fit(features_train, labels_train)
+pred1 = clf1.predict(features_test)
+
+clf2 = RandomForestClassifier()
+clf2.fit(features_train, labels_train)
+pred2 = clf2.predict(features_test)
+
+clf3 = KNeighborsClassifier()
+clf3.fit(features_train, labels_train)
+pred3 = clf3.predict(features_test)
+
+print accuracy_score(pred1, labels_test)
+print accuracy_score(pred2, labels_test)
+print accuracy_score(pred3, labels_test)
+ensemble_pred = [pred1[i] + pred2[i] + pred3[i] for i in range(len(pred1))]
+ensemble_pred = [int ( x > 1.5) for x in ensemble_pred]
+print accuracy_score(ensemble_pred, labels_test)
+
 
 
 #### initial visualization
